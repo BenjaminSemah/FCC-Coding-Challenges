f67086e1d97ee1b15cb182de8b6285dce340de24
Benjamin Semah <88531553+BenjaminSemah@users.noreply.github.com>
2022-03-10 22:28:13 +0000

    Create 01. finalSolution.js

---
+++ 01. Basic Algorithm Scripting/01. Celsius to fahrenheit/01. finalSolution.js
@@ -0,0 +1,9 @@
+// Final Solution (Celsius to Fahrenheit 01/16)
+
+function convertToF(celsius) {
+  let fahrenheit;
+      fahrenheit = (celsius * (9/5)) + 32
+  return fahrenheit;
+}
+
+convertToF(30); // 86
