f789ac0a7b302912bebb7c772a7ae03a641342f7
Benjamin Semah <88531553+BenjaminSemah@users.noreply.github.com>
2022-03-11 00:19:34 +0000

    Delete Basic Algorithm Scripting directory

--- Basic Algorithm Scripting/01. Celsius to Fahrenheit/01. finalSolution.js
+++
@@ -1,9 +0,0 @@
-// Final Solution (Celsius to Fahrenheit 01/16)
-
-function convertToF(celsius) {
-  let fahrenheit;
-      fahrenheit = (celsius * (9/5)) + 32
-  return fahrenheit;
-}
-
-convertToF(30); // 86
