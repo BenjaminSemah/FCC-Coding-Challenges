090cf5b1055b6bab27001d5a1f37b310cc8a6c83
Benjamin Semah <88531553+BenjaminSemah@users.noreply.github.com>
2022-04-27 19:29:54 +0000

    Add final solution for challenge 12

---
+++ 02. Intermediate Algorithm Scripting/12. Sum All Odd Fibonacci Numbers/01. finalSolution.js
@@ -0,0 +1,29 @@
+// 12/21 Sum All Odd Fibonacci Numbers
+
+function sumFibs(num) {
+  const fibArray = [1, 1]
+  
+  for (let i = 1; i <= num; i++) {
+    const prevNum1 = fibArray[fibArray.length-1]
+    const prevNum2 = fibArray[fibArray.length-2]
+    const fibNumber = prevNum1 + prevNum2
+
+    if (fibNumber <= num) {
+      fibArray.push(fibNumber)
+    }
+  }
+
+  const fibOddNumbers = fibArray.filter(num => {
+    return num % 2 == 1
+  })
+
+  let sum = 0
+  
+  fibOddNumbers.forEach(oddNum => {
+    sum += oddNum
+  })
+
+  return sum;
+}
+
+sumFibs(4); // 5
